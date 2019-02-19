3b14a12a52a71b93f90b621fe18b8b63e8c538af
Evelyn Cheung <ev.cheung33@gmail.com>
2019-02-19 17:38:30 -0500

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+// what is the below and what is the difference from RoutingModule?
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
@@ -9,6 +11,13 @@ import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GamesComponent } from './games/games.component';
 import { BodyComponent } from './body/body.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent},
+  // how to make default path /dashboard? Is that correct to do according to requirements? Ask Webster
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'games', component: GamesComponent },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +31,8 @@ import { BodyComponent } from './body/body.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
