d532e0e83201ce93c94935251e764a9d42f5e6e5
Evelyn Cheung <ev.cheung33@gmail.com>
2019-02-19 17:11:20 -0500

    adding nav, footer and body components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppNavComponent } from './app-nav/app-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GamesComponent } from './games/games.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     AppNavComponent,
+    FooterComponent,
+    DashboardComponent,
+    GamesComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
