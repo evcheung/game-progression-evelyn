11c2b567d644630c9e2efd7af18ed8726aff06c6
Evelyn Cheung <ev.cheung33@gmail.com>
2019-02-20 16:24:48 -0500

    added profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GamesComponent } from './games/games.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'games', component: GamesComponent },
+  { path: 'profile', component: ProfileComponent},
   { path: '**', redirectTo: '/' },
 ];
 
